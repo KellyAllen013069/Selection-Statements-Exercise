02ee535aab482296160e8688f0d8a63a413af5d5
KellyAllen013069 <89815777+KellyAllen013069@users.noreply.github.com>
2024-07-10 14:09:47 -0500

    exercise complete

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var favNumber = 88;
+            
+            Console.WriteLine("Guess the number between 1 and 100");
+            var userInput = int.Parse(Console.ReadLine());
+            if (userInput < favNumber) Console.WriteLine("Too low.");
+            else if (userInput > favNumber) Console.WriteLine("Too high");
+            else Console.WriteLine($"Congratulations! You guessed the correct number {favNumber}.");
         }
     }
 }
